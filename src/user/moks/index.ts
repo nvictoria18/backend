a0fd66405506500880e1671685f79b655396a065
Victoria <v.nesterenko@nv.dunice.net>
2024-05-21 12:32:53 +0300

    srabotalo

---
+++ src/user/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+  {
+    "_id": "664c49f3e93d6fbc4dec5e9e",
+    "index": 0,
+    "guid": "015feac5-34cc-4492-befc-e8cc6bc81ed5",
+    "isActive": true,
+    "balance": "$1,470.41",
+    "picture": "http://placehold.it/32x32",
+    "age": 27,
+    "eyeColor": "blue",
+    "name": "Mckay Brown",
+    "gender": "male",
+    "company": "GEEKOSIS",
+    "email": "mckaybrown@geekosis.com",
+    "phone": "+1 (996) 499-3586",
+    "address": "504 Mill Avenue, Belva, Kansas, 4624",
+    "about": "Velit cillum officia adipisicing minim esse anim sunt nulla non laboris nulla nostrud. Velit incididunt sunt consequat voluptate irure aliquip velit. Id ex sit velit ut laborum sit et do Lorem. Ea incididunt amet sunt adipisicing anim in voluptate occaecat ipsum ex exercitation laboris aute incididunt. Non sunt sint qui reprehenderit pariatur ad eiusmod eiusmod cupidatat aliqua ullamco commodo. Voluptate nostrud aliqua laborum nostrud laborum et consequat excepteur excepteur occaecat ea minim.\r\n",
+    "registered": "2019-04-07T02:16:06 -03:00",
+    "latitude": 61.813372,
+    "longitude": 158.11316,
+    "tags": [
+      "veniam",
+      "culpa",
+      "aute",
+      "qui",
+      "est",
+      "id",
+      "do"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Eddie Kirkland"
+      },
+      {
+        "id": 1,
+        "name": "Mcintosh Duncan"
+      },
+      {
+        "id": 2,
+        "name": "Winifred Wagner"
+      }
+    ],
+    "greeting": "Hello, Mckay Brown! You have 1 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "664c49f3f046c9d9840952a3",
+    "index": 1,
+    "guid": "2d116a1e-6c48-4414-b0a1-80c47fd449a5",
+    "isActive": false,
+    "balance": "$3,002.64",
+    "picture": "http://placehold.it/32x32",
+    "age": 34,
+    "eyeColor": "brown",
+    "name": "Zamora Sandoval",
+    "gender": "male",
+    "company": "ELECTONIC",
+    "email": "zamorasandoval@electonic.com",
+    "phone": "+1 (834) 549-3792",
+    "address": "700 Fillmore Place, Holcombe, Nevada, 6343",
+    "about": "Minim magna veniam deserunt nulla veniam cupidatat et ut. Cillum minim elit velit aute. Mollit quis incididunt labore commodo aute in esse. Magna ad officia minim duis est veniam exercitation. Nisi irure id Lorem minim est commodo enim excepteur voluptate tempor incididunt velit. Excepteur eiusmod irure excepteur est deserunt ut deserunt sint elit.\r\n",
+    "registered": "2020-12-27T07:49:44 -03:00",
+    "latitude": -53.721991,
+    "longitude": -41.832205,
+    "tags": [
+      "exercitation",
+      "magna",
+      "ea",
+      "esse",
+      "qui",
+      "commodo",
+      "non"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Salas Slater"
+      },
+      {
+        "id": 1,
+        "name": "Tammi Sharpe"
+      },
+      {
+        "id": 2,
+        "name": "Skinner Powers"
+      }
+    ],
+    "greeting": "Hello, Zamora Sandoval! You have 10 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "664c49f320108884d4d6577c",
+    "index": 2,
+    "guid": "57e73762-b4c9-41f9-93bb-b2d30f1c7683",
+    "isActive": false,
+    "balance": "$2,152.30",
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "blue",
+    "name": "Tabitha Bean",
+    "gender": "female",
+    "company": "INRT",
+    "email": "tabithabean@inrt.com",
+    "phone": "+1 (879) 544-3678",
+    "address": "616 Village Court, Needmore, Federated States Of Micronesia, 9541",
+    "about": "Velit occaecat qui enim duis. Ad qui anim ad dolor. Sint labore sunt minim incididunt. Elit sint enim sint tempor ea est ipsum aliquip ex. Nisi sunt est velit nisi nulla adipisicing do tempor. Fugiat magna ullamco proident cupidatat eu. Irure consequat ea ullamco aliquip dolor exercitation reprehenderit nisi.\r\n",
+    "registered": "2015-08-17T07:17:07 -03:00",
+    "latitude": 88.440573,
+    "longitude": 168.7649,
+    "tags": [
+      "exercitation",
+      "deserunt",
+      "occaecat",
+      "deserunt",
+      "adipisicing",
+      "irure",
+      "pariatur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Colleen Bentley"
+      },
+      {
+        "id": 1,
+        "name": "Ewing Mcbride"
+      },
+      {
+        "id": 2,
+        "name": "Weiss Richardson"
+      }
+    ],
+    "greeting": "Hello, Tabitha Bean! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "664c49f34f10ddc71ebe97be",
+    "index": 3,
+    "guid": "0f99ac73-ccae-40e2-9ea2-eb56416f7c60",
+    "isActive": false,
+    "balance": "$1,103.30",
+    "picture": "http://placehold.it/32x32",
+    "age": 27,
+    "eyeColor": "green",
+    "name": "Pam Bartlett",
+    "gender": "female",
+    "company": "UNISURE",
+    "email": "pambartlett@unisure.com",
+    "phone": "+1 (912) 515-3818",
+    "address": "763 Corbin Place, Falmouth, Alabama, 2426",
+    "about": "Cillum et aliquip in aliquip adipisicing exercitation aliquip sit ad eu. Eiusmod velit laborum in qui nisi. Magna Lorem deserunt id laborum. Officia occaecat id in fugiat aliqua est anim sint.\r\n",
+    "registered": "2017-01-14T01:58:46 -03:00",
+    "latitude": -23.042241,
+    "longitude": -104.774676,
+    "tags": [
+      "id",
+      "magna",
+      "mollit",
+      "ad",
+      "ipsum",
+      "quis",
+      "eu"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Hester Park"
+      },
+      {
+        "id": 1,
+        "name": "Richards Wood"
+      },
+      {
+        "id": 2,
+        "name": "Elisabeth Kline"
+      }
+    ],
+    "greeting": "Hello, Pam Bartlett! You have 5 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "664c49f3a1b06cfee36121f0",
+    "index": 4,
+    "guid": "9e0787e6-8882-4331-b8ae-317e6ac2d36d",
+    "isActive": true,
+    "balance": "$2,247.13",
+    "picture": "http://placehold.it/32x32",
+    "age": 26,
+    "eyeColor": "brown",
+    "name": "Krystal Pitts",
+    "gender": "female",
+    "company": "RECRISYS",
+    "email": "krystalpitts@recrisys.com",
+    "phone": "+1 (821) 463-3662",
+    "address": "110 Glenmore Avenue, Hollins, Wisconsin, 7961",
+    "about": "In aliqua nulla dolor mollit eiusmod quis mollit proident. Nulla qui veniam elit eiusmod nulla nostrud elit laborum. Officia Lorem commodo minim ut dolore eiusmod ex non.\r\n",
+    "registered": "2015-07-18T06:44:05 -03:00",
+    "latitude": 39.472572,
+    "longitude": 52.877636,
+    "tags": [
+      "nisi",
+      "laboris",
+      "sit",
+      "Lorem",
+      "eiusmod",
+      "labore",
+      "consectetur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Mcmahon Harrison"
+      },
+      {
+        "id": 1,
+        "name": "Lillian Mcclure"
+      },
+      {
+        "id": 2,
+        "name": "Cervantes Dalton"
+      }
+    ],
+    "greeting": "Hello, Krystal Pitts! You have 4 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "664c49f36dc138b2d5744797",
+    "index": 5,
+    "guid": "2e8b2665-2be9-4d76-b7bb-c345f37738b5",
+    "isActive": false,
+    "balance": "$3,567.71",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "blue",
+    "name": "Sawyer Howell",
+    "gender": "male",
+    "company": "BUZZOPIA",
+    "email": "sawyerhowell@buzzopia.com",
+    "phone": "+1 (950) 547-3053",
+    "address": "529 Noll Street, Sutton, District Of Columbia, 1647",
+    "about": "Nisi Lorem voluptate anim sunt labore laboris est enim nulla enim elit ea sit. Veniam Lorem id non sit aliquip proident ut laboris ex culpa aute. Deserunt proident irure mollit Lorem id commodo sunt aute sunt ullamco nisi labore. Eiusmod consequat ea excepteur culpa. Ut reprehenderit consequat ea incididunt ex ipsum cillum qui commodo. Qui esse velit id nulla consequat in in culpa officia elit in amet.\r\n",
+    "registered": "2022-03-09T10:17:09 -03:00",
+    "latitude": -73.387519,
+    "longitude": 142.647911,
+    "tags": [
+      "ut",
+      "elit",
+      "deserunt",
+      "nulla",
+      "est",
+      "sint",
+      "aliqua"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Nadine Burris"
+      },
+      {
+        "id": 1,
+        "name": "Ada Stanley"
+      },
+      {
+        "id": 2,
+        "name": "Hallie Beck"
+      }
+    ],
+    "greeting": "Hello, Sawyer Howell! You have 9 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "664c49f39d07bb2a3bf485d3",
+    "index": 6,
+    "guid": "ece9beda-d3d5-48f5-8290-81d77b586b34",
+    "isActive": false,
+    "balance": "$1,278.55",
+    "picture": "http://placehold.it/32x32",
+    "age": 31,
+    "eyeColor": "green",
+    "name": "Bright Bass",
+    "gender": "male",
+    "company": "OLUCORE",
+    "email": "brightbass@olucore.com",
+    "phone": "+1 (898) 480-2125",
+    "address": "683 Bills Place, Centerville, Marshall Islands, 3983",
+    "about": "Occaecat aliqua esse cupidatat quis consequat magna consequat deserunt aliquip. In reprehenderit enim duis ea proident mollit culpa ea laboris. Exercitation eu incididunt reprehenderit Lorem cillum aliqua consequat in irure.\r\n",
+    "registered": "2020-05-28T01:15:54 -03:00",
+    "latitude": 50.932421,
+    "longitude": 85.372134,
+    "tags": [
+      "irure",
+      "ipsum",
+      "amet",
+      "magna",
+      "aliqua",
+      "ipsum",
+      "et"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Alissa Perez"
+      },
+      {
+        "id": 1,
+        "name": "Craig Woodward"
+      },
+      {
+        "id": 2,
+        "name": "Tracy Middleton"
+      }
+    ],
+    "greeting": "Hello, Bright Bass! You have 3 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]
