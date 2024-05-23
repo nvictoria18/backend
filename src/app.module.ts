c10ad8888d93639a05d325dee57996afb86fe630
nvictoria18 <v.nesterenko@nv.dunice.net>
2024-05-23 19:52:30 +0300

    tasks v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from "@nestjs/config";
 import { User } from "./users/users.model";
-import { RolesModule } from './roles/roles.module';
+import { TasksModule } from './tasks/tasks.module';
+import { Task } from "./tasks/tasks.model";
 
 @Module({
   controllers: [],
@@ -17,13 +18,13 @@ import { RolesModule } from './roles/roles.module';
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: String(process.env.POSTGRES_PASSWORD),
+      password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [User],
+      models: [User, Task],
       autoLoadModels: true, 
     }),
     UsersModule,
-    RolesModule,
+    TasksModule,
   ]
 })
 export class AppModule {}
